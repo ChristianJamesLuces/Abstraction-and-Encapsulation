20f2fefd1b27d83101a577c625f2d28bccdae015
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 17:43:20 +0800

    create methods

--- Car.py
+++ Car.py
@@ -2,9 +2,21 @@
 class Car:
     #Add a Constructor
     def __init__(self, year_model, make, speed = 0 ):
-        #Create the data attributes
+        #Create data attributes
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
 
-#Create methods
+    #Create methods
+    def accelerate(self):
+        self.__speed += 5
+    
+    def brake(self):
+        self.__speed -= 5
+        if self.__speed < 0:
+            self.__speed = 0
+
+    def get_speed(self):
+        return self.__speed
+    
+    
