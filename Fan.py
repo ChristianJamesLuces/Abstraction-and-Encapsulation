0f5c8e8ff56f33dd1c6bd4c638aed03442b195b3
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 16:37:49 +0800

    create setter methods

--- Fan.py
+++ Fan.py
@@ -26,4 +26,16 @@ class Fan:
     def get_on(self):
         return self.__on
     
+    #Create setter methods
+    def set_speed(self, speed):
+        self.__speed = speed 
+
+    def set_radius(self, radius):
+        self.__radius = radius
+    
+    def set_color(self, color):
+        self.__color = color 
+    
+    def set_on(self, on):
+        self.__on = on 
     
