cc919804a16d4ae78b3a002573e309f2638ad0ff
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 17:33:53 +0800

    add a constructor and its data attributes

--- Car.py
+++ Car.py
@@ -1,3 +1,10 @@
 #Create a class named Car
-#Add a Constructor
+class Car:
+    #Add a Constructor
+    def __init__(self, year_model, make, speed = 0 ):
+        #Create the data attributes
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = speed
+
 #Create methods
