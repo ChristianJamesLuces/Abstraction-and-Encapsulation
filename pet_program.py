589f115a222d481b69b1cf48193dfa6e5ad281f5
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 20:17:48 +0800

    initialize the class

--- pet_program.py
+++ pet_program.py
@@ -1,4 +1,9 @@
 #Import the modules and files
+from pet_ui import UserInterface
+from pet import Pet
+
 #Initialize the class
+ui = UserInterface()
+pet = Pet()
 #Ask the user's input
 #Display the pet's details  
