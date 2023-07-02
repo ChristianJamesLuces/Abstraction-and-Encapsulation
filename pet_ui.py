cd12838b2854853716e4f643f9f6386098a05f4e
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 20:04:45 +0800

    ask the user input

--- pet_ui.py
+++ pet_ui.py
@@ -7,3 +7,17 @@ class Pet:
         self.pet = Pet()
 
     #Ask the user inputs
+    def pet_name(self):
+        name = input("Enter the name of your pet: ")
+        self.pet.set_name(name)
+
+    def pet_type(self):
+        type = input("Enter the type of your pet: ")
+        self.pet.set_type(type)
+    
+    def pet_age(self):
+        age = input("Enter the age of your pet: ")
+        self.pet.set_age(type)
+
+    #Display the pet details
+    
