8b5601c0d76cd067e210717f6fe2394d7c3a4ebb
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 17:54:05 +0800

    call the brake method 5 times

--- TestCar.py
+++ TestCar.py
@@ -6,6 +6,9 @@ test_car = Car(2020, "Tesla")
 #Call the accelerate method 5 times
 for i in range(5):
     test_car.accelerate()
-    print("Current speed: ", test_car.get_speed())
-    
+    print("Current Speed: ", test_car.get_speed())
+
 #Call the brake method 5 times
+for i in range(5):
+    test_car.brake()
+    print("Current Speed: ", test_car.get_speed())
