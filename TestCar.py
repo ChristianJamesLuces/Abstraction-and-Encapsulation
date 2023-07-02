e43fd9069b2c42b8a4e61a4822a200be2b88179b
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 17:52:36 +0800

    call the accelerate method 5 times

--- TestCar.py
+++ TestCar.py
@@ -1,5 +1,11 @@
 from Car import Car
 
 #Create a car object
+test_car = Car(2020, "Tesla")
+
 #Call the accelerate method 5 times
+for i in range(5):
+    test_car.accelerate()
+    print("Current speed: ", test_car.get_speed())
+    
 #Call the brake method 5 times
