09368ad1816d2e39ec6ded50eea35454aa37153e
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 16:20:05 +0800

    add a constructor

--- Fan.py
+++ Fan.py
@@ -1,10 +1,22 @@
 
-#(The Fan class).  Design a class named Fan to represent a fan. The class contains:
-#Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
-#A private int data field named speed that specifies the speed of the fan.
-#A private bool data field named on that specifies whether the fan is on (the default is False).
-#A private float data field named radius that specifies the radius of the fan.
-#A private string data field named color that specifies the color of the fan.
-#The accessor(getters)  and mutator(setters)  methods for all four data fields.
-#A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
+#class named Fan to represent a fan
+class Fan:
+    #Three constants for fan speed
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    #Add a constructor
+    def __init__(self, speed = 1, radius = 5, color = "blue", on = False ):
+        self.__speed = speed 
+        self.__radius = radius
+        self.__color = color
+        self.__on = on 
+
+#A private int data field named speed that specifies the speed of the fan
+#A private bool data field named on that specifies whether the fan is on (the default is False)
+#A private float data field named radius that specifies the radius of the fan
+#A private string data field named color that specifies the color of the fan
+#The accessor(getters)  and mutator(setters)  methods for all four data fields
+#A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False)
 
