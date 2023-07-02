7adf4c052ddba8ee7e50f2a92c911b5350863b07
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-02 16:47:00 +0800

    create the objects

--- TestFan.py
+++ TestFan.py
@@ -1,4 +1,11 @@
+from Fan import Fan
+
 #Create a class named TestFan
-#Create the objects
-#Display the properties of fan1
-#Display the properties of fan2
+class TestFan:
+    #Create the objects
+    def main(self):
+        fan1 = Fan (Fan.FAST, 10, "yellow", True)
+        fan2 = Fan (Fan.MEDIUM, 5, "blue", False)
+    #Display the properties of fan1
+
+    #Display the properties of fan2
