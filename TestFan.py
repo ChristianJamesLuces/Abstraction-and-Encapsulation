c881a31a137327fa6422846c79568dffa46d9124
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-03 14:31:18 +0800

    changing it into table

--- TestFan.py
+++ TestFan.py
@@ -5,18 +5,17 @@ from fan import Fan
 fan1 = Fan(Fan.FAST, 10, "yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
+# Define the table headings and data for fan properties
+table_headings = ["Fan", "Speed", "Radius", "Color", "On"]
+table_data = [
+    ["Fan 1", str(fan1.get_speed()), str(fan1.get_radius()), str(fan1.get_color()), str(fan1.get_on())],
+    ["Fan 2", str(fan2.get_speed()), str(fan2.get_radius()), str(fan2.get_color()), str(fan2.get_on())]
+]
+
 # Define the layout for the GUI
 layout = [
-    [sg.Text("Fan 1 Properties")],
-    [sg.Text("Speed:"), sg.InputText(str(fan1.get_speed()))],
-    [sg.Text("Radius:"), sg.InputText(str(fan1.get_radius()))],
-    [sg.Text("Color:"), sg.InputText(str(fan1.get_color()))],
-    [sg.Checkbox("On", default=fan1.get_on())],
-    [sg.Text("Fan 2 Properties")],
-    [sg.Text("Speed:"), sg.InputText(str(fan2.get_speed()))],
-    [sg.Text("Radius:"), sg.InputText(str(fan2.get_radius()))],
-    [sg.Text("Color:"), sg.InputText(str(fan2.get_color()))],
-    [sg.Checkbox("On", default=fan2.get_on())]
+    [sg.Table(values=table_data, headings=table_headings, justification="center", key="-TABLE-", num_rows=2)],
+    [sg.Button("Exit")]
 ]
 
 # Create the window
@@ -25,7 +24,7 @@ window = sg.Window("Fan Properties", layout)
 # Event loop to process GUI events
 while True:
     event, values = window.read()
-    if event == sg.WINDOW_CLOSED:
+    if event == sg.WINDOW_CLOSED or event == "Exit":
         break
 
 # Close the window
