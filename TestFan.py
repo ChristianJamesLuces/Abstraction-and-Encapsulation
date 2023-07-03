661f312f22fee046452d8dc018128fb20905f880
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-03 14:15:05 +0800

    add gui to the fan

--- TestFan.py
+++ TestFan.py
@@ -1,21 +1,32 @@
+import PySimpleGUI as sg
 from fan import Fan
 
-#Create the objects
+# Create the fan objects
 fan1 = Fan(Fan.FAST, 10, "yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
-    
-#Display the properties of fan1
-print("Fan 1: ")
-print("Speed: ", fan1.get_speed())
-print("Radius: ", fan1.get_radius())
-print("Color: ", fan1.get_color())
-print("On: ", fan1.get_on())
 
-#Display the properties of fan2
-print("\nFan 2: ")
-print("Speed: ", fan2.get_speed())
-print("Radius: ", fan2.get_radius())
-print("Color: ", fan2.get_color())
-print("On: ", fan2.get_on())
+# Define the layout for the GUI
+layout = [
+    [sg.Text("Fan 1 Properties")],
+    [sg.Text("Speed:"), sg.InputText(str(fan1.get_speed()))],
+    [sg.Text("Radius:"), sg.InputText(str(fan1.get_radius()))],
+    [sg.Text("Color:"), sg.InputText(str(fan1.get_color()))],
+    [sg.Checkbox("On", default=fan1.get_on())],
+    [sg.Text("Fan 2 Properties")],
+    [sg.Text("Speed:"), sg.InputText(str(fan2.get_speed()))],
+    [sg.Text("Radius:"), sg.InputText(str(fan2.get_radius()))],
+    [sg.Text("Color:"), sg.InputText(str(fan2.get_color()))],
+    [sg.Checkbox("On", default=fan2.get_on())]
+]
 
+# Create the window
+window = sg.Window("Fan Properties", layout)
 
+# Event loop to process GUI events
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+
+# Close the window
+window.close()
